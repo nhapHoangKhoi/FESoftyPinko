67b5c49a2bfcbb03880e2c6f0f41ca556b1b9dfb
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-11-01 16:31:50 +0700

    Synchronous button with the menu collapsing animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,35 @@ const menuMobility = document.querySelector(".header__menu-mobi");
 if(menuMobility)
 {
    const menuMobilityIcon = menuMobility.querySelector(".header__menu-mobi--button");
+   const collapseElement = document.querySelector("#pinko-menu");
 
    if(menuMobilityIcon)
    {
       menuMobilityIcon.addEventListener("click", () => 
          {
-            menuMobilityIcon.classList.toggle("open");
+            // Check if the menu is currently being toggled
+            const isCollapsing = collapseElement.classList.contains("collapsing");
+
+            if(!isCollapsing) {
+               // toggle the collapse state only if not animating
+               if (collapseElement.classList.contains("show")) {
+                  $(collapseElement).collapse('hide');
+               } 
+               else {
+                  $(collapseElement).collapse('show');
+               }
+            }
          }
       );
+
+      // event listeners for Bootstrap collapse events
+      $(collapseElement).on('show.bs.collapse', () => {
+         menuMobilityIcon.classList.add("open");
+      });
+
+      $(collapseElement).on('hide.bs.collapse', () => {
+         menuMobilityIcon.classList.remove("open");
+      });
    }
 }
 // ----- End menu mobi button
